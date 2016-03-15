6fdfd72441c61376a39d25c768127cf82efd6e82
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:03:29 +0100

    Initial commit

---
+++ Example/GPXKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GPXKit
+//
+//  Created by Jelle Vandebeeck on 03/15/2016.
+//  Copyright (c) 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
