568f49a3b07c80c0c84d42c9788ca69d50a829a2
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:37:39 +0100

    Prepare the route tests.

---
+++ Example/Tests/RouteTests.swift
@@ -0,0 +1,49 @@
+import Quick
+import Nimble
+import GPXKit
+
+class RouteSpec: QuickSpec {
+    override func spec() {
+        describe("route data") {
+            pending("should have a name") {}
+            pending("should have a comment") {}
+            pending("should have a description") {}
+            pending("should have a source") {}
+            pending("should have a link") {}
+            pending("should have a number") {}
+            pending("should have a type") {}
+        }
+        
+        describe("routes") {
+            pending("should not have routes") {}
+            pending("should have routes") {}
+        }
+        
+        describe("route point") {
+            pending("should have a coordinate") {}
+            pending("should have an elevation") {}
+            pending("should have a time") {}
+            pending("should have a magnatic variation") {}
+            pending("should have a mean sea level height") {}
+            pending("should have a name") {}
+            pending("should have a comment") {}
+            pending("should have a description") {}
+            pending("should have a source") {}
+            pending("should have a link") {}
+            pending("should have a symbol") {}
+            pending("should have a type") {}
+            pending("should have a fix") {}
+            pending("should have satelites") {}
+            pending("should have a horizontal dilution of precision") {}
+            pending("should have a vertical dilution of precision") {}
+            pending("should have a position dilution of precision") {}
+            pending("should have an age of gpx data") {}
+            pending("should have a DGPS station id") {}
+        }
+        
+        describe("route points") {
+            pending("should not have route points") {}
+            pending("should have route points") {}
+        }
+    }
+}
