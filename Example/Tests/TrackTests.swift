14f99d918a3d30c98713156cc572f5114461941e
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:40:46 +0100

    Prepare the track tests.

---
+++ Example/Tests/TrackTests.swift
@@ -0,0 +1,54 @@
+import Quick
+import Nimble
+import GPXKit
+
+class TrackSpec: QuickSpec {
+    override func spec() {
+        describe("track data") {
+            pending("should have a name") {}
+            pending("should have a comment") {}
+            pending("should have a description") {}
+            pending("should have a source") {}
+            pending("should have a link") {}
+            pending("should have a number") {}
+            pending("should have a type") {}
+        }
+        
+        describe("tracks") {
+            pending("should not have tracks") {}
+            pending("should have tracks") {}
+        }
+        
+        describe("track segments") {
+            pending("should not have track segments") {}
+            pending("should have track segments") {}
+        }
+        
+        describe("track point") {
+            pending("should have a coordinate") {}
+            pending("should have an elevation") {}
+            pending("should have a time") {}
+            pending("should have a magnatic variation") {}
+            pending("should have a mean sea level height") {}
+            pending("should have a name") {}
+            pending("should have a comment") {}
+            pending("should have a description") {}
+            pending("should have a source") {}
+            pending("should have a link") {}
+            pending("should have a symbol") {}
+            pending("should have a type") {}
+            pending("should have a fix") {}
+            pending("should have satelites") {}
+            pending("should have a horizontal dilution of precision") {}
+            pending("should have a vertical dilution of precision") {}
+            pending("should have a position dilution of precision") {}
+            pending("should have an age of gpx data") {}
+            pending("should have a DGPS station id") {}
+        }
+        
+        describe("track points") {
+            pending("should not have track points") {}
+            pending("should have track points") {}
+        }
+    }
+}
