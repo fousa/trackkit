82a44e039333ac22af697cbad16d20a0bfde3114
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:25:20 +0100

    Prepare the common tests.

---
+++ Example/Tests/CommonTests.swift
@@ -0,0 +1,22 @@
+import Quick
+import Nimble
+import GPXKit
+
+class CommonSpec: QuickSpec {
+    override func spec() {
+        describe("common data") {
+            pending("should have a creator name") {}
+            
+            context("metadata") {
+                pending("should have a name") {}
+                pending("should have a description") {}
+                pending("should have a author") {}
+                pending("should have a copyright notice") {}
+                pending("should have a link") {}
+                pending("should have a time") {}
+                pending("should have keywords") {}
+                pending("should have bounds") {}
+            }
+        }
+    }
+}
