8f6c4887c32f3892e8ad1a4c012bb28cf7098def
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:03:57 +0100

    Add pods to the commit.

---
+++ Example/Pods/Target Support Files/GPXKit/GPXKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double GPXKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char GPXKitVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-GPXKit_Tests/Pods-GPXKit_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GPXKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GPXKit_TestsVersionString[];
+
