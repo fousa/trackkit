de07e316ce6c7187493c8b1c8f11cc4809c8e719
Jelle Vandebeeck <jelle@fousa.be>
2016-03-15 23:31:17 +0100

    Prepate the waypoint tests.

---
+++ Example/Tests/WaypointTests.swift
@@ -0,0 +1,29 @@
+import Quick
+import Nimble
+import GPXKit
+
+class WaypointSpec: QuickSpec {
+    override func spec() {
+        describe("waypoint data") {
+            pending("should have a coordinate") {}
+            pending("should have an elevation") {}
+            pending("should have a time") {}
+            pending("should have a magnatic variation") {}
+            pending("should have a mean sea level height") {}
+            pending("should have a name") {}
+            pending("should have a comment") {}
+            pending("should have a description") {}
+            pending("should have a source") {}
+            pending("should have a link") {}
+            pending("should have a symbol") {}
+            pending("should have a type") {}
+            pending("should have a fix") {}
+            pending("should have satelites") {}
+            pending("should have a horizontal dilution of precision") {}
+            pending("should have a vertical dilution of precision") {}
+            pending("should have a position dilution of precision") {}
+            pending("should have an age of gpx data") {}
+            pending("should have a DGPS station id") {}
+        }
+    }
+}
