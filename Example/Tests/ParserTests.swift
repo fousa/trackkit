c3ec798654de3337b755dcaa3729204ac34f6b7a
Jelle Vandebeeck <jelle@fousa.be>
2016-03-16 10:39:20 +0100

    Prepare parser tests.

---
+++ Example/Tests/ParserTests.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+import GPXKit
+
+class ParserSpec: QuickSpec {
+    override func spec() {
+        describe("parser") {
+            pending("should be successful") {}
+            pending("should throw an parse error") {}
+            pending("should throw an empty error") {}
+        }
+    }
+}
