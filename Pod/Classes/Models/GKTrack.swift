b765a93bd43f2b442b266675f7c83cd6945a9d22
Jelle Vandebeeck <jelle@fousa.be>
2016-03-19 10:33:24 +0100

    Document the track.

--- Pod/Classes/Models/GKTrack.swift
+++ Pod/Classes/Models/GKTrack.swift
@@ -9,17 +9,35 @@
 import Foundation
 import AEXML
 
+/**
+ An ordered list of points describing a path.
+*/
 public final class GKTrack {
+    /// GPS name of track.
     public var name: String?
+
+    /// GPS comment for track.
     public var comment: String?
+
+    /// User description of track.
     public var description: String?
+
+    /// Source of data. Included to give user some idea of reliability and accuracy of data.
     public var source: String?
-    public var type: String?
-    
-    public var number: Int?
-    
+
+    /// Links to external information about track.
     public var link: GKLink?
-    
+
+    /// GPS track number.
+    public var number: Int?
+
+    /// Type (classification) of track.
+    public var type: String?
+
+    /// A Track Segment holds a list of Track Points which are logically connected in
+    /// order. To represent a single GPS track where GPS reception was lost, or the
+    /// GPS receiver was turned off, start a new Track Segment for each continuous span
+    /// of track data.
     public var segments: [GKTrackSegment]?
 }
 
