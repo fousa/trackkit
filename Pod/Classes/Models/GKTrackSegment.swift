0002875509c53cf43d35ef47831ddd55f700d08f
Jelle Vandebeeck <jelle@fousa.be>
2016-03-19 10:33:30 +0100

    Document the track segment.

--- Pod/Classes/Models/GKTrackSegment.swift
+++ Pod/Classes/Models/GKTrackSegment.swift
@@ -9,7 +9,11 @@
 import Foundation
 import AEXML
 
+/**
+ A Track Segment holds a list of Track Points which are logically connected in order. To represent a single GPS track where GPS reception was lost, or the GPS receiver was turned off, start a new Track Segment for each continuous span of track data.
+ */
 public final class GKTrackSegment {
+    /// A Track Point holds the coordinates, elevation, timestamp, and metadata for a single point in a track.
     public var points: [GKPoint]?
 }
 
