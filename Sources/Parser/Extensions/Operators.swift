64b73a1d210df2966089b30353c01d5c3171145a
Jelle Vandebeeck <jelle@fousa.be>
2016-03-21 15:54:36 +0100

    Remove useless commented code.

--- Sources/Parser/Extensions/Operators.swift
+++ Sources/Parser/Extensions/Operators.swift
@@ -49,8 +49,5 @@ func <~ (inout lhs: [String]?, rhs: AEXMLElement) { // tailor:disable
 }
 
 func <~ (inout lhs: NSDate?, rhs: AEXMLElement) { // tailor:disable
-//    if let timeString = rhs.optionalStringValue {
     lhs = rhs.optionalStringValue?.isoDate()
-//        lhs = NSDate(fromString: timeString, format: .ISO8601(nil))
-//    }
 }
