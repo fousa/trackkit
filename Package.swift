99ea68c4628cd7200895b4569a95455aa2a95f11
Jelle Vandebeeck <jelle@fousa.be>
2016-03-21 15:33:06 +0100

    Add the AEXML dependency to the package.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-  name: "GPXKit"
+  name: "GPXKit",
+  dependencies: [
+    .Package(url: "https://github.com/fousa/AEXML.git", majorVersion: 2, minor: 1)
+  ]
 )
