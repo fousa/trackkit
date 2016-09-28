47fdf30c4dd84ea56e0549c2af1656be89f67bf1
Jelle Vandebeeck <jelle@fousa.be>
2016-09-28 18:44:11 +0200

    Seperate the ParseError.

--- Sources/GPX/GPXParser.swift
+++ Sources/GPX/GPXParser.swift
@@ -11,23 +11,6 @@ import CoreLocation
 import AEXML
 
 /**
- A list of errors that can be thrown by the parser. These errors can be
- thrown during initialization or during the parsing process.
-
- - invalidData: When the data object is empty.
- - invalidFormat: When the data contains an incorrect format that can't be parsed.
- - invalidVersion: When the version of the file is invalid.
-*/
-public enum ParseError: Error {
-    /// Thrown when the data object is empty.
-    case invalidData
-    /// Thrown when the data contains an incorrect format that can't be parsed.
-    case invalidFormat
-    /// Thrown when the data contains an incorrect version that can't be parsed.
-    case invalidVersion
-}
-
-/**
  The parser that is responsible for converting a given `Data` representation of
  the GPX file into an understandable format.
 */
