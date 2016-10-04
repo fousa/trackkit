b4404c41326d5909503a949261b157dab0b55e73
Jelle Vandebeeck <jelle@fousa.be>
2016-10-04 07:19:25 +0200

    Optimize the use of the date formatters.

--- Sources/Parser/Extensions/String+DateParsing.swift
+++ Sources/Parser/Extensions/String+DateParsing.swift
@@ -7,17 +7,29 @@
 extension String {
 
     func isoDate() -> Date? {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        formatter.timeZone = TimeZone(abbreviation: "UTC")
-        return formatter.date(from: self)
+        return DateFormatter.isoDateFormatter.date(from: self)
     }
 
     func isoDateWithMilliseconds() -> Date? {
+        return DateFormatter.isoDateFormatterWithMilliseconds.date(from: self)
+    }
+
+}
+
+extension DateFormatter {
+
+    fileprivate static let isoDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        return formatter
+    }()
+
+    fileprivate static let isoDateFormatterWithMilliseconds: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         formatter.timeZone = TimeZone(abbreviation: "UTC")
-        return formatter.date(from: self)
-    }
+        return formatter
+    }()
 
 }
