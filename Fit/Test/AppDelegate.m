a263fb31f6b0f4a2e645ca3eea5cbe5d821d9e3b
Jelle Vandebeeck <jelle@fousa.be>
2018-01-07 19:39:45 +0100

    Add some sample files.

---
+++ Fit/Test/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  Test
+//
+//  Created by Jelle Vandebeeck on 07/01/2018.
+//  Copyright © 2018 Jelle Vandebeeck. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+@end
