3c100cf96e8c09757c55e18b1d857371ee3ac1ee
Jelle Vandebeeck <jelle@fousa.be>
2018-01-07 18:43:37 +0100

    Add initial fit project.

---
+++ Fit/Fit/SomeClass.mm
@@ -0,0 +1,21 @@
+//
+//  Some.m
+//  AEXML
+//
+//  Created by Jelle Vandebeeck on 26/12/2017.
+//
+
+#import "SomeClass.h"
+
+#import "FitDecode.h"
+
+@implementation SomeClass
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSLog(@"Hello there from `SomeClass` 👋");
+    }
+    return self;
+}
+
+@end
