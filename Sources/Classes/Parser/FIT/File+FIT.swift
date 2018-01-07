cdf79cae18ae62f2ea8cd4f6b8d9b48d3be6fe57
Jelle Vandebeeck <jelle@fousa.be>
2018-01-07 23:27:18 +0100

    Add basic activity parsing.

--- Sources/Classes/Parser/FIT/File+FIT.swift
+++ Sources/Classes/Parser/FIT/File+FIT.swift
@@ -5,6 +5,8 @@
 //  Created by Jelle Vandebeeck on 10/12/2017.
 //
 
+import Fit
+
 extension File {
     
     // MARK: - Init
@@ -13,6 +15,25 @@ extension File {
         // Fetch the type version.
         let version = try TrackTypeVersion(type: .fit, version: "1.0")
         self.init(type: .fit, version: version)
+        
+        let reader = FitReader(data: data)
+        reader?.read()
+        
+        if let records = reader?.records, records.count > 0 {
+            let activity = Activity()
+            let lap = Lap()
+            
+            lap.points = records.flatMap {
+                let point = Point()
+                if let record = $0 as? FitRecord {
+                    point.coordinate = record.coordinate
+                }
+                return point
+            }
+            
+            activity.laps = [lap]
+            activities = [activity]
+        }
     }
     
 }
